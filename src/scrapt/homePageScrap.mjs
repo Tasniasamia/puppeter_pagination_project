33dff77cafe52e497cd65a1bd62ca7d2b1decc81
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-07-30 09:39:11 +0600

    work on first project

---
+++ src/scrapt/homePageScrap.mjs
@@ -0,0 +1,54 @@
+import puppeteer from "puppeteer";
+
+const main = async (url) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  let collectedArray=[];
+  while (true) {
+    // প্রোডাক্ট গুলো count করো
+    const products = await page.$$eval('[class*="box product"]', (productEls) => {
+      let temp = [];
+      productEls.forEach((el) => {
+        temp.push({
+          name: el.querySelector(".product-title")?.innerText || "",
+          link: el.querySelector("a")?.href || "",
+          price: el.querySelector(".price")?.innerText || "",
+        });
+      });
+      return temp; // বাইরে পাঠালাম
+    });
+    
+
+    
+    collectedArray.push(...products);
+    console.log("Products on this page:", collectedArray.length);
+
+    // Next page আছে কিনা চেক করো
+    const nextSelector = 'ul.pagination li a[rel="next"], ul.pagination li a[title="layout.pagination.next_html"]';
+    const hasNext = await page.$(nextSelector);
+    if (!hasNext) {
+      console.log("No more pages. Scraping complete!");
+      break;
+    }
+
+    const prevCount = products.length;
+
+    // Click করো
+    await page.click(nextSelector);
+
+    // এখন products update হওয়া পর্যন্ত wait করো (full navigation এর বদলে)
+    await page.waitForFunction(
+      (selector, prev) => document.querySelectorAll(selector).length !== prev,
+      { timeout: 60000 },
+      '[class*="box product"]',
+      prevCount
+    );
+  }
+  console.log("collectedArray",collectedArray);
+
+  await browser.close();
+};
+
+await main("https://www.mollyjogger.com/collections/inventory");
